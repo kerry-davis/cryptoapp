d6d71fc801a1b31068e739ce03ab40afe8901654
kerry-davis <pulsta@gmail.com>
2021-11-19 16:54:14 +1300

    git corrupt had to remove and start from scratch

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsApiHeaders = {
+	"x-bingapis-sdk": "true",
+	"x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
+	"x-rapidapi-host": process.env.REACT_APP_CRYPTONEWS_RAPIDAPI_HOST,
+}
+
+const baseUrl = process.env.REACT_APP_BASEURL_NEWS
+
+const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders})
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query ({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),    })
+  })
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi
