8a38fa8cefd6e48932659c841792885329ee095f
kerry-davis <pulsta@gmail.com>
2021-11-22 10:15:43 +1300

    add LogRocket

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
+import LogRocket from 'logrocket';
 
 import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
 import './App.css'
 
 const App = () => {
+  LogRocket.init('erxyrz/cryptoviewapp');
   return (
     <div className="app">
       <div className="navbar">
