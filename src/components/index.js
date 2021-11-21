11e3d0e7297a4ab51742941ddac9db4398aaf060
kerry-davis <pulsta@gmail.com>
2021-11-22 11:17:29 +1300

    remove ScrollToTop.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,3 @@ export { default as News } from './News';
 export { default as Cryptocurrencies } from './Cryptocurrencies';
 export { default as Exchanges } from './Exchanges';
 export { default as CryptoDetails } from './CryptoDetails';
-// export { default as ScrollToTop } from './ScrollToTop';
